13b50364fea43c97536b79c696cdca7d76c5660b
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-04-04 18:29:55 +0100

    functionality

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
-let menu = document.querySelector('.doughnut')
+let links = document.querySelector('.links')
+let minus = document.querySelector('.minus')
+let plus = document.querySelector('.plus')
+let counter = document.querySelector('.zero')
+let cartImg = document.querySelector('.cartImg')
+let button = document.querySelector('.btnDiv')
+let wares = document.querySelector('.wares')
+let wareprice = document.querySelector('.wareprice')
+let stories = document.querySelector('.stories')
 
-function menuList() {
-    document.body.classList.toggle('link-div')
-}
+
+minus.addEventListener('click', function () {
+    minus = Number(counter.innerText--)
+})
+
+plus.addEventListener('click', function () {
+    plus = Number(counter.innerText++)
+})
+
+button.addEventListener('click', function () {
+    wares = wares.textContent;
+    console.log(wares);
+})
+
+cartImg.addEventListener('click', function () {
+    let sum = counter.textContent * wareprice.textContent
+    console.log(sum);
+})
