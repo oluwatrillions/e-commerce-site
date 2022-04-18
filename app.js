2eb2adda39cd5396e86a6307b533193eac690ca3
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-04-18 11:22:25 +0100

    checkout

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ let button = document.querySelector('.btnDiv')
 let wares = document.querySelector('.wares')
 let wareprice = document.querySelector('.wareprice')
 let stories = document.querySelector('.stories')
+let showcart = document.querySelector('.showCart')
+let itemCount = document.querySelector('.itemCount')
+let linkin = document.querySelectorAll('.linkin')
+let itemPrices = document.querySelector('.itemPrices').innerText
+let itemSymbol = document.querySelector('.itemSymbol')
+let numOfItem = document.querySelector('.numOfItem').innerText
+let itemTotal = document.querySelector('.itemTotal').innerText
+let checkout = document.querySelector('.checkout')
 
 
 minus.addEventListener('click', function () {
@@ -18,11 +26,27 @@ plus.addEventListener('click', function () {
 })
 
 button.addEventListener('click', function () {
-    wares = wares.textContent;
-    console.log(wares);
+    wares = counter.textContent;
+    itemCount.textContent = wares
 })
 
 cartImg.addEventListener('click', function () {
-    let sum = counter.textContent * wareprice.textContent
-    console.log(sum);
+   showcart.classList.toggle('showCartItem')
 })
+
+linkin.forEach((e) => {
+    e.addEventListener('click', (e) => {
+        let linkTarget = e.currentTarget
+        linkTarget.style.color = 'red';
+    })
+})
+
+checkout.addEventListener('click', () => {
+    itemPrices = Number(itemPrices)
+    numOfItem = Number(numOfItem)
+    document.querySelector('.itemTotal').innerText = itemPrices * numOfItem
+    console.log(itemTotal);
+    console.log(itemPrices);
+    console.log(numOfItem);
+})
+
