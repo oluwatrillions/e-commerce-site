049552209de7c6873cb5b24554d8a3819f3d4faf
Ajose Michael Oluwatobi <ajosemichaeloluwatobi@yahoo.com>
2022-04-20 15:51:27 +0100

    cart btn

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let minus = document.querySelector('.minus')
 let plus = document.querySelector('.plus')
 let counter = document.querySelector('.zero')
 let cartImg = document.querySelector('.cartImg')
-let button = document.querySelector('.btnDiv')
+let button = document.querySelectorAll('.btnDiv')
 let itemDetails = document.querySelector('.itemDetail')
 let wareprice = document.querySelector('.wareprice')
 let stories = document.querySelector('.stories')
@@ -12,7 +12,13 @@ let itemCount = document.querySelector('.itemCount')
 let linkin = document.querySelectorAll('.linkin')
 let itemSymbol = document.querySelector('.itemSymbol')
 let checkout = document.querySelector('.checkout')
-let cartCount = document.querySelector('.cartCount')
+let price = document.querySelector('.price')
+
+linkin.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        linkin.classList.add('linkDecor')
+    })
+})
 
 minus.addEventListener('click', function () {
     minus = Number(counter.innerText--)
@@ -22,13 +28,17 @@ plus.addEventListener('click', function () {
     plus = Number(counter.innerText++)
 })
 
-button.addEventListener('click', function (e) {
-    showcart.classList.toggle('showCartItem');
-    
-    if (e.target.classList.contains('cartDiv')) {
-        alert('hey')
-    }
 
+button.forEach((btn) => {
+
+  btn.addEventListener('click', function (e) {
+    showcart.classList.toggle('showCartItem');
+        console.log(e.target);
+
+        if (e.target.parentElement.parentElement.parentElement.classList.contains('price')){
+            console.log(e.target.parentElement.parentElement.previousElementSibling.children[0].children[0].innerText);
+        }
+        
     itemDetail = itemDetails.innerText
     itemPrices = wareprice.innerText
     numOfItem = counter.innerText
@@ -36,9 +46,9 @@ button.addEventListener('click', function (e) {
     document.querySelector('.carth4').innerText = itemDetail
     document.querySelector('.itemPrices').innerText = itemPrices
     document.querySelector('.numOfItem').innerText = numOfItem
-    document.querySelector('.itemTotal').innerText = itemTotal
+    document.querySelector('.itemTotal').innerText = itemTotal 
+})
 
-    
 })
 
 linkin.forEach((e) => {
@@ -51,4 +61,11 @@ linkin.forEach((e) => {
 checkout.addEventListener('click', () => {
   
 })
+
+const cartList = () => {
+    const cartItemList = {}
+
+
+
+}
 
